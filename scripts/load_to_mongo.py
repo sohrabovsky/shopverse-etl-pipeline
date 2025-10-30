e6eb77e851db7c50a8afd56fa5f7187239c8b0cf
Sohrab Salehin <sohrab@Sohrabs-MacBook-Pro.local>
2025-10-30 16:13:57 +0100

    Some name corrections in files

--- scripts/load_to_mongo.py
+++ scripts/load_to_mongo.py
@@ -1,28 +1,28 @@
 import os
-csv_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..", "data", "raw", "products.csv"))
+csv_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..", "data", "orders.csv"))
 print("Resolved CSV path:", csv_path)
 print("Exists:", os.path.exists(csv_path))
 
 import pandas as pd
 from pymongo import MongoClient
 
-# 📁 تعیین مسیر مطلق فایل CSV
+# Findig CSV for raw data
 base_dir = os.path.abspath(os.path.dirname(__file__))
-csv_path = os.path.join(base_dir, "..", "..", "data", "raw", "products.csv")
+csv_path = os.path.join(base_dir, "..", "..", "data", "raw", "orders.csv")
 
 print("📄 CSV Path:", csv_path)
 print("✅ File Exists:", os.path.exists(csv_path))
 
-# 🧠 اتصال به MongoDB
+# Connecting to Mongo
 client = MongoClient("mongodb://localhost:27017/")
 db = client["shopverse"]
-collection = db["products"]
+collection = db["orders"]
 
-# 📥 خواندن CSV
+# Reading CSV
 df = pd.read_csv(csv_path, encoding='utf-8', sep=",")
 records = df.to_dict(orient="records")
 
-# 💾 درج در MongoDB
+# Loading to MongoDB
 collection.insert_many(records)
 
 print(f"✅ {len(records)} records were imported into MongoDB.")
